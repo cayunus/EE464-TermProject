86c100296fc7acf17ae36880fb23fc51d3edc21f
cayunus <cayunus@metu.edu.tr>
2021-04-27 00:39:23 +0300

    SkinDepth, AWG and resistance calculations added
    Skin depth calculation according to frequency and temperature has been added. Also some resistance calculation are added based on selection of AWG of cable

--- Cayunus/MatlabCodes/CoreSelection.m
+++ Cayunus/MatlabCodes/CoreSelection.m
@@ -14,3 +14,22 @@ Bmax = (N*Ipeak*u)/le; % Tesla - Needed max B (Flux density)
 % Saturation current can also calculated from the formula below
 Isat_other = (Bsat*le)/(N*u);
 Isat = (Bsat*Ae)/sqrt(Lm*AL); % A - Saturation current of the core
+%% Winding
+% Main aim of the calculation of skin depth is to choose cable having
+% radius as thin as skin depth value for the sake of avoid to use thick
+% cables in vain.
+% Copper relative permeability = 1;
+uCu = 1*u0; % Henry/meter - Permeability of copper
+TCu = 80; % Celcius - Temperature of Cu
+roCu = (1.68e-8)*(1+(TCu-25)*4.29e-3); % Ohm-meter - Resistivity of Cu at T degree
+SigmaCu = 1/roCu; % siemens/meter - Conductivity of copper
+fs = 100e3; % Hz - Switching frequency
+SkinDepth = 1/sqrt(fs*pi*SigmaCu*uCu)*1e3; % mm- Theor. skin depth
+SkinDepth_OzanHSlide = [(7.5e-2)/sqrt(fs)]*1e3; % mm - Skin depth at 100°C Ozan Hoca Slide
+r_awg = (0.455e-3)/2; % AWG 25 
+% Resistance of 1m cable with consideration of skin depth
+if (SkinDepth >= r_awg) % Compare the cable mm and SkinDepth
+    R_Skin = (roCu*1)/(pi*((r_awg^2)));
+else
+    R_Skin = (roCu*1)/(pi*((r_awg^2)-((r_awg-SkinDepth)^2)));
+end
